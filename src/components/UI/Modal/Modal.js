302026b499e96d04e08cf9b21f92151e503daf28
zulimazuli <zulimazuli@gmail.com>
2020-09-10 20:04:57 +0200

    Backdrop and modal window with buttons

--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -1,17 +1,23 @@
 import React from 'react';
-
+import Aux from '../../../hoc/ReactAux';
+import Backdrop from '../Backdrop/Backdrop';
 import styles from './Modal.module.css';
 
 const modal = (props) => {
-    return (
-        <div className={styles.Modal}
+  return (
+    <Aux>
+        <Backdrop show={props.show} clicked={props.modalClosed} />
+      <div
+        className={styles.Modal}
         style={{
-            transform: props.show ? 'translateY(0)' : 'translateY(-100vh)',
-            opacity: props.show ? '1' : '0'
-        }}>
-            {props.children}
-        </div>
-    )
-}
+          transform: props.show ? 'translateY(0)' : 'translateY(-100vh)',
+          opacity: props.show ? '1' : '0',
+        }}
+      >
+        {props.children}
+      </div>
+    </Aux>
+  );
+};
 
 export default modal;
--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -64,6 +64,14 @@ class BurgerBuilder extends Component {
     this.setState({purchasing: true});
   }
 
+  purchaseCancelHandler = () => {
+    this.setState({purchasing: false});
+  }
+  
+  purchaseContinueHandler = () => {
+    alert('You continue!');
+  }
+
   render() {
     const disabledInfo = { ...this.state.ingredients };
     for (let key in disabledInfo) {
@@ -72,8 +80,12 @@ class BurgerBuilder extends Component {
 
     return (
       <Aux>
-        <Modal show={this.state.purchasing}>
-          <OrderSummary ingredients={this.state.ingredients} />
+        <Modal show={this.state.purchasing} modalClosed={this.purchaseCancelHandler}>
+          <OrderSummary 
+            ingredients={this.state.ingredients} 
+            price={this.state.totalPrice}
+            purchaseCancelled={this.purchaseCancelHandler}
+            purchaseContinued={this.purchaseContinueHandler} />
         </Modal>
         <Burger ingredients={this.state.ingredients} />
         <BuildControls
