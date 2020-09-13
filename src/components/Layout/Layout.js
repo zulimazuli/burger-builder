1c91feb2c3db518686e5d6589bbdcba94c6d73e3
zulimazuli <zulimazuli@gmail.com>
2020-09-13 21:10:14 +0200

    Toolbar and Navigation components

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import Aux from '../../hoc/ReactAux';
+import Toolbar from '../Navigation/Toolbar';
 
 import styles from './Layout.module.css';
 
 const layout = (props) => (
   <Aux>
-    <div>toolbar</div>
+    <Toolbar />
     <main className={styles.Content}>{props.children}</main>
   </Aux>
 );
