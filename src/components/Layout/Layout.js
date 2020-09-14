2c4adbf2deb71bae3d8f287be01da96c8fe1da78
zulimazuli <zulimazuli@gmail.com>
2020-09-14 21:13:18 +0200

    Side Drawer menu

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,14 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Aux from '../../hoc/ReactAux';
 import Toolbar from '../Navigation/Toolbar';
+import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
 
 import styles from './Layout.module.css';
 
-const layout = (props) => (
-  <Aux>
-    <Toolbar />
-    <main className={styles.Content}>{props.children}</main>
-  </Aux>
-);
+const Layout = (props) => {
+  const [showSideDrawer, setShowSideDrawer] = useState(false);
 
-export default layout;
+  // const prevShowSideDrawerRef = useRef(null);
+  
+  // useEffect(() => {
+  //   prevShowSideDrawerRef.current = showSideDrawer;
+  // });
+
+  // const prevShowSideDrawer = prevShowSideDrawerRef.current;
+
+  return (
+    <Aux>
+      <Toolbar drawerToggleClicked={() => setShowSideDrawer(!showSideDrawer)} />
+      <SideDrawer
+        open={showSideDrawer}        
+        closed={() => setShowSideDrawer(false)}
+      />
+      <main className={styles.Content}>{props.children}</main>
+    </Aux>
+  );
+};
+
+export default Layout;
