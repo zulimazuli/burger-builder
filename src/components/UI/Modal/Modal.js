bb38eb238e0ace36a91d746b11658f30d322c803
Tomasz Nowakowski <tnowak@softserveinc.com>
2020-10-20 14:17:52 +0200

    Added Memo to Modal component

--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -1,12 +1,17 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import Aux from '../../../hoc/ReactAux';
 import Backdrop from '../Backdrop/Backdrop';
 import styles from './Modal.module.css';
 
-const modal = (props) => {
+const Modal = (props) => {
+  //for debug
+  useEffect(() => {
+    console.log('[Modal] has changed.');
+  });
+
   return (
     <Aux>
-        <Backdrop show={props.show} clicked={props.modalClosed} />
+      <Backdrop show={props.show} clicked={props.modalClosed} />
       <div
         className={styles.Modal}
         style={{
@@ -20,4 +25,8 @@ const modal = (props) => {
   );
 };
 
-export default modal;
+const areEqual = (prevProps, nextProps) => {
+  return nextProps.show === prevProps.show;
+}
+
+export default React.memo(Modal, areEqual);
